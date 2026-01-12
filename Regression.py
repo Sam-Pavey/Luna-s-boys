89d1e21e6ac17193a42ac9a96b4354f00daf1503
Sam Pavey <69520480+Sam-Pavey@users.noreply.github.com>
2026-01-12 16:47:39 +0000

    Refactor regression.py for 2023 model analysis
    Updated regression analysis to include only 2023 model and added comments for clarity.

--- Regression.py
+++ Regression.py
@@ -5,6 +5,9 @@ Created on Sun Dec 14 19:52:31 2025
 @author: ssamp
 """
 
+
+# this file can be altered easily to include different years in the regression (only 2026, 2018, 2023 work as they have the relevant variables. For our regression we used only 2023. 
+
 from statsmodels.formula.api import ols
 from statsmodels.iolib.summary2 import summary_col
 import pandas as pd 
@@ -26,20 +29,21 @@ df[['religion']]= df[['Religion or denomination belonging to at present, United 
 # m2010= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=d2010).fit()
 # m2012= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=df[df['Year']==2012]).fit()
 # m2014= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=df[df['Year']==2014]).fit()
-m2016= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=df[df['Year']==2016]).fit()
-m2018= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=df[df['Year']==2018]).fit()
+# m2016= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=df[df['Year']==2016]).fit()
+# m2018= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=df[df['Year']==2018]).fit()
 m2023= ols("l_r ~ log_news + emotional_country + emotional_europe + C(religion, Treatment(reference='Not applicable'))", data=df[df['Year']==2023]).fit()
 
 
 
 
 table=summary_col( # create a regression table 
-    [m2016, m2018, m2023], # pass the models to the summary_col function
+    [m2023], # pass the models to the summary_col function
     stars=True, # add stars denoting the p-values of the coefficient to the table; * p<0.05, ** p<0.01, *** p<0.001
     float_format='%0.3f', # set the decimal places to 3
-    model_names=['2016', '2018', '2023'], # set the name of the model
+    model_names=['2023'], # set the name of the model
     info_dict = {"N":lambda x: "{0:d}".format(int(x.nobs))}) # add the number of observations to the table
 
 # print(table.as_csv())
 # print(table)
+
 print(table.as_latex())
